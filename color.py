7b64a6bf6ca65d3a636483add41dcbbd9feda8f6
Frittierapparat <72391367+Frittierapparat@users.noreply.github.com>
2022-05-15 00:51:22 +0200

    Add accuracy
    Added the wished accuracy to the function, which scales down the image based on the percentage provided. Cuts down on execution time.

--- color.py
+++ color.py
@@ -1,21 +1,32 @@
-def avgImgColor(path):
+def avgImgColor(path, accuracy):
     from PIL import Image
-    import matplotlib.pyplot as plt
+    if accuracy > 100:
+        print("Accuracy set to above 100%, defaulting to 100%!")
+        accuracy = 100
+    elif accuracy <=0:
+        print("Accuracy set to 0% or smaller, defaulting to 1%!")
+        accuracy = 1
+
     r,g,b = 0, 0, 0
 
     img = Image.open(path)
     img = img.convert("RGB")
+    width = int(img.size[0] * (accuracy / 100))
+    height = int(img.size[1] * (accuracy / 100))
+    width, height = img.size
+    img.thumbnail((width, height), Image.ANTIALIAS)
     pixels = img.load()
 
-    for x in range(img.size[0]):
-        for y in range(img.size[1]):
+    for x in range(width):
+        for y in range(height):
             r += pixels[x,y][0]/255
             g += pixels[x,y][1]/255
             b += pixels[x,y][2]/255
     
-    r /= img.size[0] * img.size[1]
-    g /= img.size[0] * img.size[1]
-    b /= img.size[0] * img.size[1]
+    r /= width * height
+    g /= width * height
+    b /= width * height
 
     return (r, g, b)
+
 
